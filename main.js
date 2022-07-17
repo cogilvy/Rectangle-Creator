3f796727c9ce3f83bb0f26dc779bcec4023f33a4
cogilvy <chrisogilvy@cox.net>
2022-07-16 21:56:51 -0700

    Drag to move & resize working

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const applyColorBtn = document.getElementById('apply-color');
 mainEl.addEventListener('mousedown', handleMouseDown);
 mainEl.addEventListener('mousemove', handleMouseMove);
 mainEl.addEventListener('mouseup', handleMouseUp);
+mainEl.addEventListener('click', selectRectangle);
 
 // State Variables
 let currentRect;
@@ -20,12 +21,15 @@ let startY;
 let endX;
 let endY;
 let isMouseDown;
+let isResizing;
+let isDragging;
 let layout = [];
+
 
 
 // Functions
 function handleMouseDown(evt) {
-  if (evt.target.classList.contains('rectangle')) return;
+  if (evt.target.classList.contains('rectangle') || isDragging || isResizing) return;
   const idNum = document.querySelectorAll('.rectangle').length+1;
   isMouseDown = true;
   startX = evt.pageX;
@@ -40,7 +44,7 @@ function handleMouseDown(evt) {
 }
   
 function handleMouseMove(evt) {
-  if (!isMouseDown) return;
+  if (!isMouseDown || isDragging || isResizing) return;
   endX = evt.pageX;
   endY = evt.pageY;
   currentRect.style.width = `${endX - startX}px`;
@@ -48,10 +52,130 @@ function handleMouseMove(evt) {
 }
 
 function handleMouseUp(evt) {
-  if (!isMouseDown) return;
+  if (!isMouseDown || isDragging || isResizing) return;
   let rect = {id: currentRect.id, startX, startY, endX, endY, color: colorSelector.value};
   layout.push(rect);
   isMouseDown = false;
   currentRect = null;
-  console.log(layout);
+}
+
+
+
+
+
+
+
+// SELECT & DRAG RECTANGLE
+function selectRectangle(evt) {
+  if (!evt.target.classList.contains('rectangle')) return;
+  selectedRect = evt.target;
+  selectedRect.classList.add('selected');
+  const allRectangles = document.querySelectorAll('.rectangle');
+  allRectangles.forEach(rect => {
+    if (rect.id !== evt.target.id) {
+      rect.classList.remove('selected');
+      rect.innerHTML = '';
+    }
+  });
+  selectedRect.addEventListener('mousedown', handleMouseDownDRAG);
+  selectedRect.innerHTML = `
+    <div class="resizer ne"></div>
+    <div class="resizer nw"></div>
+    <div class="resizer se"></div>
+    <div class="resizer sw"></div>`;
+
+  const resizers = document.querySelectorAll('.resizer');
+  let currentResizer;
+  
+  resizers.forEach(resizer => {
+    resizer.addEventListener('mousedown', handleMouseDown);
+  
+    function handleMouseDown(evt) {
+      isResizing = true;
+      currentResizer = evt.target;
+  
+      let prevX = evt.pageX;
+      let prevY = evt.pageY;
+  
+      window.addEventListener('mousemove', handleMouseMove);
+      window.addEventListener('mouseup', handleMouseUp);
+  
+      function handleMouseMove(evt) {
+        const rect = selectedRect.getBoundingClientRect();
+  
+        if (currentResizer.classList.contains('se')) {
+          selectedRect.style.width = `${rect.width - (prevX - evt.pageX)}px`;
+          selectedRect.style.height = `${rect.height - (prevY - evt.pageY)}px`;
+        } 
+        else if (currentResizer.classList.contains('sw')) {
+          selectedRect.style.width = `${rect.width + (prevX - evt.pageX)}px`;
+          selectedRect.style.height = `${rect.height - (prevY - evt.pageY)}px`;
+          selectedRect.style.left = `${rect.left - (prevX - evt.pageX)}px`;
+        } 
+        else if (currentResizer.classList.contains('ne')) {
+          selectedRect.style.width = `${rect.width - (prevX - evt.pageX)}px`;
+          selectedRect.style.height = `${rect.height + (prevY - evt.pageY)}px`;
+          selectedRect.style.top = `${rect.top - (prevY - evt.pageY)}px`;
+        } 
+        else if (currentResizer.classList.contains('nw')) {
+          selectedRect.style.width = `${rect.width + (prevX - evt.pageX)}px`;
+          selectedRect.style.height = `${rect.height + (prevY - evt.pageY)}px`;
+          selectedRect.style.left = `${rect.left - (prevX - evt.pageX)}px`;
+          selectedRect.style.top = `${rect.top - (prevY - evt.pageY)}px`;
+        }
+        prevX = evt.pageX;
+        prevY = evt.pageY;
+      }
+  
+      function handleMouseUp() {
+        window.removeEventListener('mousemove', handleMouseMove);
+        window.removeEventListener('mouseup', handleMouseUp);
+        isResizing = false;
+      }
+    }
+  });
+  
+
+  function handleMouseDownDRAG(evt) {
+    mainEl.removeEventListener('mouseup', handleMouseUp);
+    if (isResizing) return;
+    isDragging = true;
+    // We are adding these event listeners to the entire window, because sometimes
+    // our mouse might not be dragging on top of the div.
+    window.addEventListener('mousemove', handleMouseMoveDRAG);
+    window.addEventListener('mouseup', handleMouseUpDRAG);
+  
+    // evt.pageX/Y is the location of the mouse on the X/Y axis
+    let prevX = evt.pageX;
+    let prevY = evt.pageY;
+  
+    function handleMouseMoveDRAG(evt) {
+      if (!isDragging) return;
+      let newX = prevX - evt.pageX;
+      let newY = prevY - evt.pageY;
+  
+      const rect = selectedRect.getBoundingClientRect();
+      const conRect = mainEl.getBoundingClientRect();
+      const newLeft = rect.left - newX;
+      const newTop = rect.top - newY;
+      if (
+        newLeft === conRect.left ||
+        newTop === conRect.top
+        ) {
+          return handleMouseUpDRAG();
+        }
+  
+      selectedRect.style.left = `${newLeft}px`; // 200 - (-1) = 201   MOVED RIGHT
+      selectedRect.style.top = `${newTop}px`;   // 150 - 1 = 149      MOVED UP 
+      prevX = evt.pageX;
+      prevY = evt.pageY;
+    }
+  
+    function handleMouseUpDRAG(evt) {
+      window.removeEventListener('mousemove', handleMouseMoveDRAG);
+      window.removeEventListener('mouseup', handleMouseUpDRAG);
+      mainEl.addEventListener('mouseup', handleMouseUp);
+      isDragging = false;
+    }
+  }
 }
